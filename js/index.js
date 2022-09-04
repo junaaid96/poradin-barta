bd93cd8a0cf2839dfad10445d77faf2cb2bb598d
junaaid96 <info.junaidulislam@gmail.com>
2022-09-04 21:06:29 +0600

    modal information loaded

--- js/index.js
+++ js/index.js
@@ -37,8 +37,8 @@ const displayNews = allNews => {
     allNewsCard.textContent = '';
     allNews.forEach(news => {
         const allNewsDiv = document.createElement('div');
-        allNewsDiv.innerHTML = 
-        `
+        allNewsDiv.innerHTML =
+            `
         <div class="row g-0 my-3 border p-2 rounded-3">
             <div class="col-md-4">
                 <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
@@ -46,7 +46,7 @@ const displayNews = allNews => {
             <div class="col-md-8">
                 <div class="card-body">
                     <h5 class="card-title">${news.title}</h5>
-                    <p class="card-text my-3">${news.details.slice(0,300)}</p>
+                    <p class="card-text my-3">${news.details.slice(0, 300)}</p>
                     <p class="card-text text-truncate my-3">${news.details.slice(300)}</p>
                     <div class="d-flex justify-content-between mt-5">
                         <div class="d-flex">
@@ -54,7 +54,7 @@ const displayNews = allNews => {
                             <p class="mx-3">${news.author.name}</p>
                         </div>
                         <p><i class="fa-regular fa-eye"></i> ${news.total_view}</p>
-                        <button onclick="displayNewsModal('${news._id}')" type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#newsModal">View News</button> 
+                        <button onclick="loadNewsModal('${news._id}')" type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#newsModal">View News</button> 
                     </div>          
                 </div>
             </div>
@@ -62,14 +62,43 @@ const displayNews = allNews => {
         `;
         allNewsCard.appendChild(allNewsDiv);
     });
-    
+
 }
 
-const displayNewsModal = viewNews => {
-    console.log(viewNews);
+const loadNewsModal = async newsID => {
+    const url = `https://openapi.programming-hero.com/api/news/${newsID}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayNewsModal(data.data);
 }
 
-
+const displayNewsModal = allNewsModal => {
+    const modalSection = document.getElementById('view-news-modal');
+    allNewsModal.forEach(newsModal => {
+        const modalDiv = document.createElement('div');
+        modalDiv.innerHTML =
+            `
+        <div class="modal-header">
+            <h5 class="modal-title" id="newsModalLabel">${newsModal.title}</h5>
+        </div>
+        <div class="modal-body">
+            <p class="card-text my-3">${newsModal.details.slice(0, 300)}</p>
+            <div class="d-flex justify-content-between mt-5">
+                <div class="d-flex">
+                    <img src="${newsModal.author.img}" class="rounded-circle" alt="..." width="40" height="40">
+                    <p class="mx-3">${newsModal.author.name}</p>
+                </div>
+                <p><i class="fa-regular fa-eye"></i> ${newsModal.total_view}</p>
+            </div>    
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Close</button>
+        </div>
+        `;
+        modalSection.appendChild(modalDiv);
+        console.log(newsModal.title);
+    });
+}
 
 loadNews(8);
 
